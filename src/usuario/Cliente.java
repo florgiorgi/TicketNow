744d89604d7979b7620ce355e34dac021aae023e
florgiorgi <mgiorgi@itba.edu.ar>
2017-10-14 18:31:09 -0300

    1.1

--- src/usuario/Cliente.java
+++ src/usuario/Cliente.java
@@ -1,108 +1,86 @@
 package usuario;
 
+import java.util.Set;
 import espectaculo.Espectaculo;
-import espectaculo.Funcion;
-import compra.Compra;
-import fecha.Hora;
 import fecha.Fecha;
+import espectaculo.Funcion;
+import fecha.Hora;
 
-import java.util.ArrayList;
-import java.util.List;
+public class Proveedor extends Cliente {
 
-public class Cliente {
+	private String nombreProv;
+	private String categoria;
+	private Set<Espectaculo> espectaculos;
+	
+	public Proveedor(String nombre, String apellido, String usuario, String mail, String fechaNac, String DNI, String contraseña) {
+		super(nombre, apellido, usuario, mail, fechaNac, DNI, contraseña);
+	}
 
-	private String nombre;
-	private String apellido;
-	private String usuario;
-	private String mail;
-	private String fechaNac;
-	private String DNI;
-	private String contraseña;
-	private List<Compra> compras;
+	public String getNombreProv() {
+		return this.nombreProv;
+	}
 
-	public Cliente(String nombre, String apellido, String usuario, String mail, String fechaNac, String DNI, String contraseña) {
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.usuario = usuario;
-		this.mail = mail;
-		this.fechaNac = fechaNac;
-		this.DNI = DNI;
-		this.contraseña = contraseña;
-		this.compras = new ArrayList<Compra>();
+	public String getCategoria() {
+		return this.categoria;
+	}
+
+	public Set<Espectaculo> getEspectaculos() {
+		return this.espectaculos;
+	}
+
+	public void agregarEspectaculo(Espectaculo esp) {
+		espectaculos.add(esp);
 	}
 	
-	public String getNombre() {
-		return this.nombre;
-	}
-
-	public String getApellido() {
-		return this.apellido;
-	}
-
-	public String getUsuario() {
-		return this.usuario;
-	}
-
-	public String getMail() {
-		return this.mail;
-	}
-
-	public String getFechaNac() {
-		return this.fechaNac;
-	}
-
-	public String getDNI() {
-		return this.DNI;
-	}
-
-	public String getContraseña() {
-		return this.contraseña;
+	//devuelve true si pudo borrar el espectaculo
+	public boolean eliminarEspectaculo(Espectaculo esp) {
+		return espectaculos.remove(esp);
 	}
 	
-	public List<Compra> getCompras() {
-		return this.compras;
-	}
-	
-	public void modificarDatos(String nombre, String apellido, String mail, String fechaNac, String DNI, String contraseña) {
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.mail = mail;
-		this.fechaNac = fechaNac;
-		this.DNI = DNI;
-		this.contraseña = contraseña;
-	}
-	
-	//devuelve true si se pudo realizar la compra
-	public boolean comprar(Espectaculo esp, Funcion func, Integer dia, Integer mes, Integer año, Integer horas, Integer minutos, Integer cantEntradas) {
-		Hora hora = new Hora(horas, minutos);
-		Fecha fecha = new Fecha(dia, mes, año);
-		
-		Compra compra = new Compra(this, esp, func, fecha, hora, cantEntradas);
-		
-		if(pagar(compra)) {
-			compras.add(compra);
-			return true;
-		} else {
-			return false;
+	//devuelve true si pudo modificar el espectaculo
+	public boolean modificarEspectaculo(Espectaculo esp, String nombre, String descripcion, String categoria, Fecha estreno) {
+		for(Espectaculo e : espectaculos) {
+			if(e.equals(esp)) {
+				return e.modificarEspectaculo(nombre, descripcion, categoria, estreno);
+			}
 		}
+		return false;
 	}
 	
-	//devuelve true si se pudo modificar la compra
-	public boolean modificarCompra(Compra compra, Espectaculo esp, Funcion func, Integer cantEntradas) {
-		if(compra == null)
-			throw new IllegalArgumentException();
-		return compra.modificarCompra(esp, func, cantEntradas);
+	//devuelve true si pudo agregar la funcion
+	public boolean agregarFuncion(Espectaculo esp, Funcion func) {
+		for(Espectaculo e : espectaculos) {
+			if(e.equals(esp)) {
+				return e.agregarFuncion(func);
+			}
+		}
+		return false;
 	}
 	
-	//devuelve true si se aprobó el pago
-	public boolean pagar(Compra compra) {
-		//Llama al sistema de pago externo
-		return true;
+	//devuelve true si pudo eliminar la funcion
+	public boolean eliminarFuncion(Espectaculo esp, Funcion func) {
+		for(Espectaculo e : espectaculos) {
+			if(e.equals(esp)) {
+				return e.eliminarFuncion(func);
+			}
+		}
+		return false;
+	}
+	
+	//devuelve true si pudo modificar la funcion
+	public boolean modificarFuncion(Espectaculo esp, Funcion funcion, Hora hora, String sala, Double precio, Integer cantidadDisp) {
+		for(Espectaculo e : espectaculos) {
+			if(e.equals(esp)) {
+				return e.modificarFuncion(funcion, hora, sala, precio, cantidadDisp);
+			}
+		}
+		return false;
 	}
 	
 	@Override
 	public String toString() {
-		return this.nombre + " " + this.apellido + " " + this.compras;
+		super.toString();
+		return "\n" + "Espectaculos: " + this.espectaculos;
 	}
-
+	
 }
