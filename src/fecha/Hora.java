9db38cea090f70404897cdf3116ce407100be4d9
florgiorgi <mgiorgi@itba.edu.ar>
2017-10-14 18:30:27 -0300

    1.1

--- src/fecha/Hora.java
+++ src/fecha/Hora.java
@@ -27,6 +27,37 @@ public class Hora {
 	}
 	
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((horas == null) ? 0 : horas.hashCode());
+		result = prime * result + ((minutos == null) ? 0 : minutos.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Hora other = (Hora) obj;
+		if (horas == null) {
+			if (other.horas != null)
+				return false;
+		} else if (!horas.equals(other.horas))
+			return false;
+		if (minutos == null) {
+			if (other.minutos != null)
+				return false;
+		} else if (!minutos.equals(other.minutos))
+			return false;
+		return true;
+	}
+	
+	@Override
 	public String toString() {
 		return this.horas.toString() + ":" + this.minutos.toString();
 	}
