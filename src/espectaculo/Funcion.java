7c873d43fad0918522ab1247d261a31c332ac8e1
florgiorgi <mgiorgi@itba.edu.ar>
2017-10-14 18:27:26 -0300

    1.1

--- src/espectaculo/Funcion.java
+++ src/espectaculo/Funcion.java
@@ -6,10 +6,10 @@ public class Funcion {
 
 	private Hora hora;
 	private String sala;
-	private Integer precio;
+	private Double precio;
 	private Integer cantidadDisp;
 	
-	public Funcion(Hora hora, String sala, Integer precio, Integer cantidadDisp) {
+	public Funcion(Hora hora, String sala, Double precio, Integer cantidadDisp) {
 		if(hora == null || sala == null || precio == null || cantidadDisp == null)
 			throw new IllegalArgumentException();
 		if(precio <= 0 || cantidadDisp <= 0)
@@ -28,7 +28,7 @@ public class Funcion {
 		return this.sala;
 	}
 
-	public Integer getPrecio() {
+	public Double getPrecio() {
 		return this.precio;
 	}
 
@@ -37,7 +37,7 @@ public class Funcion {
 	}
 	
 	//devuelve true si se pudo modificar la funcion
-	public boolean modificarFuncion(Hora hora, String sala, Integer precio, Integer cantidadDisp) {
+	public boolean modificarFuncion(Hora hora, String sala, Double precio, Integer cantidadDisp) {
 		if(hora == null || sala == null || precio == null || cantidadDisp == null)
 			return false;
 		if(precio <= 0 || cantidadDisp <= 0)
@@ -47,6 +47,42 @@ public class Funcion {
 		this.precio = precio;
 		this.cantidadDisp = cantidadDisp;
 		return true;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Funcion other = (Funcion) obj;
+		if (hora == null) {
+			if (other.hora != null)
+				return false;
+		} else if (!hora.equals(other.hora))
+			return false;
+		if (sala == null) {
+			if (other.sala != null)
+				return false;
+		} else if (!sala.equals(other.sala))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((hora == null) ? 0 : hora.hashCode());
+		result = prime * result + ((sala == null) ? 0 : sala.hashCode());
+		return result;
+	}
+	
+	@Override
+	public String toString() {
+		return "Hora: " + this.hora.toString() + " Sala: " + this.sala;
 	}
 	
 }
