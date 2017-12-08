44f2b52a4213ce244403101ac77ec9702e0c8647
florgiorgi <mgiorgi@itba.edu.ar>
2017-12-08 15:31:23 -0300

    Update FechaEstrenoInvalidaException.java

--- TicketNow/src/espectaculo/FechaEstrenoInvalidaException.java
+++ TicketNow/src/espectaculo/FechaEstrenoInvalidaException.java
@@ -2,7 +2,7 @@ package espectaculo;
 
 public class FechaEstrenoInvalidaException extends RuntimeException {
 
-	private static final long serialVersionUID = 8L;
+	private static final long serialVersionUID = 26L;
 	
 	public FechaEstrenoInvalidaException(String msg) {
 		super(msg);
